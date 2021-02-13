6e1b5fe7e6d8bf878d8785f0d983275a0e52fe4c
= <za.qa@outlook.com>
2021-02-13 01:23:24 +0000

    done

--- models/nodemailer.js
+++ models/nodemailer.js
@@ -16,7 +16,7 @@ function sendEmail(userEmail, subject, text) {
     to: `${userEmail}`, // list of receivers
     subject: `${subject}`, // Subject line
     // text: `<img src=""/> ${text}`, // plain text body
-    html: `${text} <br/> <br/> <img src="https://i.imgur.com/E4SBeVw.png" width="700px" height="auto"/> <br/> <br/> If you would like to stop recieving these emails, please go to https://societly.netlify.app and unsubscribe from your profile settings`, // html body
+    html: `${text} <br/> <br/> <img src="https://i.imgur.com/E4SBeVw.png" width="700px" height="auto"/> <br/> <br/> If you would like to stop recieving these emails, please go to <a>https://societly.netlify.app</a> and unsubscribe from your profile settings`, // html body
     //check if we can link to a html file
     // backticks html: `<img />`
   };
